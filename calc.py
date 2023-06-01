140a1c294d24a373f091ac6468ca8805dff39c47
IljaNoskov <99073996+IljaNoskov@users.noreply.github.com>
2023-06-01 18:06:23 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,66 +1,21 @@
-from tkinter import *
+def calc_f(string):
+    ans = eval(string)
+    if isinstance(ans, tuple):
+        print("use '.' with double and float number, not ','.")
+        raise TypeError
+    print(f'Ansver is {ans}')
+    return ans
 
 
-class Main(Frame):
-    def __init__(self, root):
-        super(Main, self).__init__(root)
-        self.build()
-
-    def build(self):
-        self.formula = "0"
-        self.lbl = Label(text=self.formula, font=("Times New Roman", 21, "bold"), bg="#000", foreground="#FFF")
-        self.lbl.place(x=11, y=50)
-
-        btns = [
-            "C", "DEL", "*", "=",
-            "1", "2", "3", "/",
-            "4", "5", "6", "+",
-            "7", "8", "9", "-",
-            "(", "0", ")", "X^2"
-        ]
-
-        x = 10
-        y = 140
-        for bt in btns:
-            com = lambda x=bt: self.logicalc(x)
-            Button(text=bt, bg="#FFF",
-                   font=("Times New Roman", 15),
-                   command=com).place(x=x, y=y,
-                                      width=115,
-                                      height=79)
-            x += 117
-            if x > 400:
-                x = 10
-                y += 81
-
-    def logicalc(self, operation):
-        if operation == "C":
-            self.formula = ""
-        elif operation == "DEL":
-            self.formula = self.formula[0:-1]
-        elif operation == "X^2":
-            self.formula = str((eval(self.formula))**2)
-        elif operation == "=":
-            self.formula = str(eval(self.formula))
-        else:
-            if self.formula == "0":
-                self.formula = ""
-            self.formula += operation
-        self.update()
-
-    def update(self):
-        if self.formula == "":
-            self.formula = "0"
-        self.lbl.configure(text=self.formula)
-
-
-if __name__ == '__main__':
-    root = Tk()
-    root["bg"] = "#000"
-    root.geometry("485x550+200+200")
-    root.title("Калькулятор")
-    root.resizable(False, False)
-    app = Main(root)
-    app.pack()
-    root.mainloop()
+def calc_run():
+    print("Enter 'end' to stop run")
+    print("Enter your str to calculate:")
+    st = input()
+    n = 0
+    while st != 'end':
+        calc_f(st)
+        print("Enter your str to calculate:")
+        st = input()
+        n += 1
+    print(f'Thanks for use our calc. You had been use it {n} times')
     
